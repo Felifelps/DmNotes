5351e83166afc321704e70049e7c12d945561c33
Felifelps <felipefelipe23456@gmail.com>
2025-06-09 19:57:45 -0300

    Requirements and devops

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ps7&pfa&lqkk1j0f)*+9&w(p)z*s4*xcjtw!bw3f)_l$5p7x2'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = int(config('DJANGO_DEBUG'))
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
